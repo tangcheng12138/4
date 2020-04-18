eb49f5dbbceb11e733d2624fb2978633d94a2657
tangcheng12138 <1043763638@qq.com>
2020-04-18 23:00:37 +0800

    good

--- js/2.js
+++ js/2.js
@@ -5,12 +5,20 @@ var pointThree=document.getElementById('point3');
 
 pointOne.onclick=function () {
     optionSelect.className= 'optionSelect1';
-}
+};
 
 pointTwo.onclick=function () {
     optionSelect.className= 'optionSelect2';
-}
+};
 
 pointThree.onclick=function () {
     optionSelect.className= 'optionSelect3';
+};
+
+window.onscroll=function () {
+    if(window.scrollY>0){
+        topNavBar.classList.add('topAnimation');
+    }else {
+        topNavBar.classList.remove('topAnimation');
+    }
 }
